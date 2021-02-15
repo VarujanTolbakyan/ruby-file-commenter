bc85272e85c6ee4e16d6f0a179dd59f3f1e81b8d
Varujan Tolbakyan <varuj.tolbakyan@gmail.com>
2021-02-15 19:00:02 +0400

    Refs #1/changed contents of test_source.rb and test_result.rb

--- test_result.rb
+++ test_result.rb
@@ -1,6 +1,41 @@
+# method 'foo' parameter(s): (a, b)
+def foo(a, b)
+end
+
+# class Foo
+class Foo
+  private
+
+  # private method 'private_method_1' parameter(s): (param1)
+  def private_method_1(param1)
+  end
+
+  private
+
+  # private method 'private_method_2' parameter(s): (param1, param2, param3)
+  def private_method_2(param1, param2, param3)
+  end
+
+  protected
+
+  # protected method 'protected_method_1'
+  def protected_method_1
+  end
+
+  public
+
+  # public method 'public_method_1'
+  def public_method_1
+  end
+
+  # public method 'public_method_2'
+  def public_method_2
+  end
+end
+
 # module Admins
+# nested classes: User
 # nested modules: UserBase
-# nested classes: User, Property
 module Admins
   # module UserBase
   module UserBase
@@ -12,37 +47,22 @@ module Admins
         @last_name = last_name
       end
 
-      class << self
-        # class method 'my_class_method' parameter(s): (num)
-        def my_class_method(num)
-          puts num
-        end
-
-        # class method 'counter' parameter(s): (obj)
-        def counter(obj)
-          obj += 1
-          obj
-        end
-      end
-
       # public method 'full_name'
       def full_name
         @first_name + @last_name
       end
 
-      # private method 'job'
+      # public method 'job'
       def job
         #------#---
       end
 
-      # protected method 'age'
+      private
+
+      # private method 'age'
       def age
         ##-------#
       end
-
-      protected :age
-
-      private :job
     end
 
     # public method 'owner'
@@ -55,199 +75,117 @@ module Admins
       #---------#------
     end
   end
+end
 
-  # class Property inherited from BaseProperty
-  class Property < BaseProperty
-    # class method 'exchange_price' parameter(s): (from, to)
-    def self.exchange_price(from, to)
-      #.....#----------
-    end
+# method 'foo1' parameter(s): (a, b)
+def foo1(a, b)
+end
 
-    # public method 'price=' parameter(s): (new_price)
-    def price=(new_price)
-      self.default_price = new_price.to_s.remove(',').presence
-    end
+# module ABC
+# nested classes: Bar, XYZError, Resource, Store
+# nested modules: XYZ, ClassMethods, Nested
+module ABC
+  # class Bar inherited from ABCBase
+  class Bar < ABCBase
+    private
 
-    # public method 'short_description'
-    def short_description
-      [name, location.short_text,].map(&:presence).compact.join(' ')
-    end
-
-    # public method 'listed_by_owner?'
-    def listed_by_owner?
-      role.owner?
-    end
-
-    # public method 'save_version?'
-    def save_version?
-      accepted?
-    end
-
-    # public method 'applyed_for_verification?'
-    def applyed_for_verification?
-      %i(verification_requested verified).include?(verification_status.to_sym)
-    end
-
-    # public method 'fact_sheet_present?'
-    def fact_sheet_present?
-      fields.fact_sheet.ordered.any?
-    end
-
-    # public method 'field' parameter(s): (name)
-    def field(name)
-      fields.find { |f| f.handle.to_sym == name.to_sym }
-    end
-
-    # public method 'formatted_field' parameter(s): (name)
-    def formatted_field(name)
-      field(name).try(:formatted)
-    end
-
-    # public method 'set_field!' parameter(s): (handle:, **args)
-    def set_field!(handle:, **args)
-      field = assign_field(handle: handle, **args)
-
-      if field.persisted?
-        if field.value.present?
-          field.save!
-        else
-          field.destroy
-        end
-      else
-        field.save! if field.value.present?
-      end
-
-      field
-    end
-
-    # public method 'assign_field' parameter(s): (handle:, **args)
-    def assign_field(handle:, **args)
-      field = field(handle) || build_field(handle: handle)
-      field.attributes = args
-      field
-    end
-
-    # public method 'recalculate_data_completeness!'
-    def recalculate_data_completeness!
-      update_columns(
-        data_completeness_pct: data_completeness_calculator.percentage,
-        data_completeness_category: data_completeness_calculator.category
-      )
-    end
-
-    # public method 'exchange_price_to_eur!'
-    def exchange_price_to_eur!
-      new_eur_price = price.exchange_to(:eur).cents
-
-      return if price_eur_cents == new_eur_price
-
-      if price_eur_cents && new_eur_price < price_eur_cents
-        PropertyPriceReducedWorker.prepare_execution self, previous_changes
-      end
-
-      update_columns price_eur_cents: new_eur_price
-    end
-
-    # public method 'perform_pipedrive_worker'
-    def perform_pipedrive_worker
-      Pipedrive::PropertyWorker.perform_async id
-    end
-
-    # public method 'internal?'
-    def internal?
-      instance_of? Property
-    end
-
-    # public method 'available_by_sales_status?'
-    def available_by_sales_status?
-      !UNAVAILABLE_BY_SALES_STATUSES.include?(sales_status)
-    end
-
-    # public method 'uk?'
-    def uk?
-      instance_of? UKRealla::Property
-    end
-
-    # public method 'funda?'
-    def funda?
-      instance_of? NLFunda::Property
-    end
-
-    # public method 'display' parameter(s): (unauthorized_placeholder = nil)
-    def display(unauthorized_placeholder = nil)
-      @display ||= Properties::DisplayableInfo.new self, unauthorized_placeholder
-    end
-
-    # public method 'csv_row'
-    def csv_row
-      data['csv_row']
+    # private method 'private_method_3'
+    def private_method_3
     end
 
     protected
 
-    # protected method 'build_field' parameter(s): (handle:, **args)
-    def build_field(handle:, **args)
-      schema = PropertyFields::Schema.field(property_type.handle, handle)
-      schema ||= { fact_sheet: true }
-
-      fields.build(
-        handle: handle,
-        position: schema[:position],
-        **args,
-        fact_sheet: schema[:fact_sheet],
-        translate: schema[:translate],
-        type: PropertyFields::Schema.type_klass(schema[:type])
-      )
+    # protected method 'protected_method_2'
+    def protected_method_2
     end
+  end
 
-    # protected method 'set_currency'
-    def set_currency
-      self.currency ||= (partner.try(:currency) || Currency.default) if new_record?
-    end
-
-    # protected method 'attach_market_data'
-    def attach_market_data
-      if saved_changes[:location_id] || saved_changes[:property_type_id]
-        MarketDataWorker.perform_async(nil, id)
+  # module XYZ
+  module XYZ
+    # module ClassMethods
+    module ClassMethods
+      # public method 'foo'
+      def foo
       end
     end
 
-    # protected method 'set_consorto_fee'
-    def set_consorto_fee
-      return if consorto_fee.present? && editable_consorto_fee?
+    class << self
+      private
 
-      self.consorto_fee = consorto_fee_calculator.fee
+      # private class method 'private_class_method'
+      def private_class_method
+      end
+
+      public
+
+      # class method 'public_class_method'
+      def public_class_method
+      end
     end
 
-    private
+    # class XYZError inherited from StandardError 
+    class XYZError < StandardError; end
 
-    # private method 'reset_listing_user_fee'
-    def reset_listing_user_fee
-      return unless role_id_changed?
-      self.brokerage_fee_sharing_user = 0 if role.owner?
-    end
+    # module Nested
+    module Nested
+      # class method 'nested_module_method'
+      def self.nested_module_method
+      end
 
-    # private method 'consorto_fee_calculator'
-    def consorto_fee_calculator
-      @consorto_fee_calculator ||= Properties::ConsortoFeesCalculator.new(self)
-    end
+      # class Resource
+      class Resource
+        # class method 'public_class_method'
+        def self.public_class_method
+        end
 
-    # private method 'data_completeness_calculator'
-    def data_completeness_calculator
-      @data_completeness_calculator ||= Properties::DataCompletenessCalculator.new(self)
-    end
+        # private method 'other_private_method'
+        def other_private_method
+        end
 
-    # private method 'editable_consorto_fee?'
-    def editable_consorto_fee?
-      !role_id_changed? && role.owner?
-    end
+        private :other_private_method
 
-    # private method 'notify_admin_about_change'
-    def notify_admin_about_change
-      notify = accepted? && User.current.present? && !User.current.admin? &&
-        (saved_changes? || fields.find(&:saved_changes?))
-      return unless notify
-      AdminMailer.property_changes(id).deliver_later wait: 1.minute
+        public
+
+        # public method 'other_public_method'
+        def other_public_method
+        end
+
+        private
+
+        # private class method 'private_class_method' parameter(s): (param)
+        def self.private_class_method(param)
+        end
+
+        # class Store
+        class Store
+          protected
+
+          # protected method 'protected_setter=' parameter(s): (p)
+          def protected_setter=(p)
+          end
+
+          public
+
+          # public method 'property_1'
+          def property_1
+          end
+
+          # public method 'setter=' parameter(s): (property)
+          def setter=(property)
+          end
+
+          # public method 'add' parameter(s): (object)
+          def add(object)
+            push << self
+          end
+        end
+
+        public
+
+        # public method 'pubilic_method'
+        def pubilic_method
+        end
+      end
     end
   end
 end
--- test_source.rb
+++ test_source.rb
@@ -1,20 +1,37 @@
+def foo(a, b)
+end
+
+class Foo
+  private
+
+  def private_method_1(param1)
+  end
+
+  private
+
+  def private_method_2(param1, param2, param3)
+  end
+
+  protected
+
+  def protected_method_1
+  end
+
+  public
+
+  def public_method_1
+  end
+
+  def public_method_2
+  end
+end
+
 module Admins
   module UserBase
     class User
       def initialize(first_name, last_name)
         @first_name = first_name
         @last_name = last_name
-      end
-
-      class << self
-        def my_class_method(num)
-          puts num
-        end
-
-        def counter(obj)
-          obj += 1
-          obj
-        end
       end
 
       def full_name
@@ -25,13 +42,11 @@ module Admins
         #------#---
       end
 
+      private
+
       def age
         ##-------#
       end
-
-      protected :age
-
-      private :job
     end
 
     def owner
@@ -42,169 +57,91 @@ module Admins
       #---------#------
     end
   end
+end
 
-  class Property < BaseProperty
-    def self.exchange_price(from, to)
-      #.....#----------
-    end
+def foo1(a, b)
+end
 
-    def price=(new_price)
-      self.default_price = new_price.to_s.remove(',').presence
-    end
+module ABC
+  class Bar < ABCBase
+    private
 
-    def short_description
-      [name, location.short_text,].map(&:presence).compact.join(' ')
-    end
-
-    def listed_by_owner?
-      role.owner?
-    end
-
-    def save_version?
-      accepted?
-    end
-
-    def applyed_for_verification?
-      %i(verification_requested verified).include?(verification_status.to_sym)
-    end
-
-    def fact_sheet_present?
-      fields.fact_sheet.ordered.any?
-    end
-
-    def field(name)
-      fields.find { |f| f.handle.to_sym == name.to_sym }
-    end
-
-    def formatted_field(name)
-      field(name).try(:formatted)
-    end
-
-    def set_field!(handle:, **args)
-      field = assign_field(handle: handle, **args)
-
-      if field.persisted?
-        if field.value.present?
-          field.save!
-        else
-          field.destroy
-        end
-      else
-        field.save! if field.value.present?
-      end
-
-      field
-    end
-
-    def assign_field(handle:, **args)
-      field = field(handle) || build_field(handle: handle)
-      field.attributes = args
-      field
-    end
-
-    def recalculate_data_completeness!
-      update_columns(
-        data_completeness_pct: data_completeness_calculator.percentage,
-        data_completeness_category: data_completeness_calculator.category
-      )
-    end
-
-    def exchange_price_to_eur!
-      new_eur_price = price.exchange_to(:eur).cents
-
-      return if price_eur_cents == new_eur_price
-
-      if price_eur_cents && new_eur_price < price_eur_cents
-        PropertyPriceReducedWorker.prepare_execution self, previous_changes
-      end
-
-      update_columns price_eur_cents: new_eur_price
-    end
-
-    def perform_pipedrive_worker
-      Pipedrive::PropertyWorker.perform_async id
-    end
-
-    def internal?
-      instance_of? Property
-    end
-
-    def available_by_sales_status?
-      !UNAVAILABLE_BY_SALES_STATUSES.include?(sales_status)
-    end
-
-    def uk?
-      instance_of? UKRealla::Property
-    end
-
-    def funda?
-      instance_of? NLFunda::Property
-    end
-
-    def display(unauthorized_placeholder = nil)
-      @display ||= Properties::DisplayableInfo.new self, unauthorized_placeholder
-    end
-
-    def csv_row
-      data['csv_row']
+    def private_method_3
     end
 
     protected
 
-    def build_field(handle:, **args)
-      schema = PropertyFields::Schema.field(property_type.handle, handle)
-      schema ||= { fact_sheet: true }
-
-      fields.build(
-        handle: handle,
-        position: schema[:position],
-        **args,
-        fact_sheet: schema[:fact_sheet],
-        translate: schema[:translate],
-        type: PropertyFields::Schema.type_klass(schema[:type])
-      )
+    def protected_method_2
     end
+  end
 
-    def set_currency
-      self.currency ||= (partner.try(:currency) || Currency.default) if new_record?
-    end
-
-    def attach_market_data
-      if saved_changes[:location_id] || saved_changes[:property_type_id]
-        MarketDataWorker.perform_async(nil, id)
+  module XYZ
+    module ClassMethods
+      def foo
       end
     end
 
-    def set_consorto_fee
-      return if consorto_fee.present? && editable_consorto_fee?
+    class << self
+      private
 
-      self.consorto_fee = consorto_fee_calculator.fee
+      def private_class_method
+      end
+
+      public
+
+      def public_class_method
+      end
     end
 
-    private
+    class XYZError < StandardError; end
 
-    def reset_listing_user_fee
-      return unless role_id_changed?
-      self.brokerage_fee_sharing_user = 0 if role.owner?
-    end
+    module Nested
+      def self.nested_module_method
+      end
 
-    def consorto_fee_calculator
-      @consorto_fee_calculator ||= Properties::ConsortoFeesCalculator.new(self)
-    end
+      class Resource
+        def self.public_class_method
+        end
 
-    def data_completeness_calculator
-      @data_completeness_calculator ||= Properties::DataCompletenessCalculator.new(self)
-    end
+        def other_private_method
+        end
 
-    def editable_consorto_fee?
-      !role_id_changed? && role.owner?
-    end
+        private :other_private_method
 
-    def notify_admin_about_change
-      notify = accepted? && User.current.present? && !User.current.admin? &&
-        (saved_changes? || fields.find(&:saved_changes?))
-      return unless notify
-      AdminMailer.property_changes(id).deliver_later wait: 1.minute
+        public
+
+        def other_public_method
+        end
+
+        private
+
+        def self.private_class_method(param)
+        end
+
+        class Store
+          protected
+
+          def protected_setter=(p)
+          end
+
+          public
+
+          def property_1
+          end
+
+          def setter=(property)
+          end
+
+          def add(object)
+            push << self
+          end
+        end
+
+        public
+
+        def pubilic_method
+        end
+      end
     end
   end
 end
